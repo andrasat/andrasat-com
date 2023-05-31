476785342f950f1d68f276beafae60e162893e74
andrasat <andra.satria1@gmail.com>
2023-05-31 19:02:24 +0700

    initial setup

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/eslint-module',
+    [
+      '@nuxtjs/tailwindcss',
+      {
+        injectPosition: {
+          after: 'https://unpkg.com/sanitize.css'
+        }
+      }
+    ]
+  ]
+})
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'tailwindcss'
+
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      colors: {
+        aero: '#51b4d3',
+        licorice: '#0e0311',
+        'medium-slate-blue': '#7d66dd',
+        coral: '#f98c67',
+        'indigo-dye': '#1c4666'
+      }
+    }
+  }
+}
