e9911ccdc131c938c51635d15479c3d6865b3fdf
andra <andra.satria1@gmail.com>
2023-10-10 17:35:08 +0700

    feat: update styling for nav menu

--- components/HeaderMenu.vue
+++ components/HeaderMenu.vue
@@ -4,10 +4,8 @@ const isOpened = shallowRef(false)
 
 const headerMenuClass = computed(() => ({
   // opened
-  'bg-gray-100': isOpened.value,
   'justify-center': isOpened.value,
   // closed
-  'border-2': !isOpened.value,
   'border-gray-300': !isOpened.value,
   'justify-evenly': !isOpened.value
 }))
@@ -16,20 +14,14 @@ const span0Class = computed(() => ({
   // opened
   transform: isOpened.value,
   'rotate-45': isOpened.value,
-  'origin-center': isOpened.value,
-  'border-t-licorice': isOpened.value,
-  // closed
-  'border-t-gray-300': !isOpened.value
+  'origin-center': isOpened.value
 }))
 
 const span1Class = computed(() => ({
   // opened
   transform: isOpened.value,
   '-rotate-45': isOpened.value,
-  'origin-center': isOpened.value,
-  'border-t-licorice': isOpened.value,
-  // closed
-  'border-t-gray-300': !isOpened.value
+  'origin-center': isOpened.value
 }))
 
 const navMenuClass = computed(() => ({
@@ -40,8 +32,6 @@ const navMenuClass = computed(() => ({
   'right-2': isOpened.value,
   'p-2': isOpened.value,
   'text-right': isOpened.value,
-  'bg-gray-100': isOpened.value,
-  'text-licorice': isOpened.value,
   'w-40': isOpened.value,
   rounded: isOpened.value
 }))
@@ -53,12 +43,12 @@ function onClick () {
 
 <template>
   <div ref="headerMenu" class="cursor-pointer my-auto h-10 w-10 rounded flex flex-col items-center transition-colors duration-150 z-10" :class="headerMenuClass" @click="onClick">
-    <span id="span0" class="transition-transform duration-150 inline-block h-px w-6 border-t" :class="span0Class" />
-    <span id="span1" class="transition-transform duration-150 inline-block h-px w-6 border-t" :class="span1Class" />
+    <span id="span0" class="transition-transform duration-150 inline-block h-px w-6 border-t border-t-gray-300" :class="span0Class" />
+    <span id="span1" class="transition-transform duration-150 inline-block h-px w-6 border-t border-t-gray-300" :class="span1Class" />
     <span v-if="!isOpened.valueOf()" class="inline-block h-px w-6 border-t border-t-gray-300" />
   </div>
 
-  <div v-if="isOpened.valueOf()" :class="navMenuClass">
+  <div v-if="isOpened.valueOf()" class="text-gray-300" :class="navMenuClass">
     <ul>
       <li>
         <NuxtLink to="#about">
