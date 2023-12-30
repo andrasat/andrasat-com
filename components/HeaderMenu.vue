4644b2dedf780a2ae522f8f9eac4f2ff793a50fc
andrasat <andra.satria1@gmail.com>
2023-12-30 23:45:07 +0700

    feat: use html5 popover for headermenu component

--- components/HeaderMenu.vue
+++ components/HeaderMenu.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-const headerMenu = ref()
-const isOpened = shallowRef(false)
-
 const navItems = [
   {
     name: 'About',
@@ -20,54 +17,16 @@ const navItems = [
     url: '#contact'
   }
 ]
-
-const headerMenuClass = computed(() => ({
-  // opened
-  'justify-center': isOpened.value,
-  // closed
-  'border-gray-300': !isOpened.value,
-  'justify-evenly': !isOpened.value
-}))
-
-const span0Class = computed(() => ({
-  // opened
-  transform: isOpened.value,
-  'rotate-45': isOpened.value,
-  'origin-center': isOpened.value
-}))
-
-const span1Class = computed(() => ({
-  // opened
-  transform: isOpened.value,
-  '-rotate-45': isOpened.value,
-  'origin-center': isOpened.value
-}))
-
-const navMenuClass = computed(() => ({
-  // opened
-  'animate-fade-in': isOpened.value,
-  absolute: isOpened.value,
-  'top-16': isOpened.value,
-  'right-2': isOpened.value,
-  'p-2': isOpened.value,
-  'text-right': isOpened.value,
-  'w-40': isOpened.value,
-  rounded: isOpened.value
-}))
-
-function onClick () {
-  isOpened.value = !isOpened.value
-}
 </script>
 
 <template>
-  <div ref="headerMenu" class="cursor-pointer my-auto h-10 w-10 rounded flex flex-col items-center transition-colors duration-150 z-10" :class="headerMenuClass" @click="onClick">
-    <span id="span0" class="transition-transform duration-150 inline-block h-px w-6 border-t border-t-gray-300" :class="span0Class" />
-    <span id="span1" class="transition-transform duration-150 inline-block h-px w-6 border-t border-t-gray-300" :class="span1Class" />
-    <span v-if="!isOpened.valueOf()" class="inline-block h-px w-6 border-t border-t-gray-300" />
-  </div>
+  <button popovertarget="nav-menu" class="my-auto h-10 w-10 rounded flex flex-col items-center justify-center z-10">
+    <span class="h-px w-4 border-t border-t-gray-300 mb-[5px]" />
+    <span class="h-px w-4 border-t border-t-gray-300 mb-[5px]" />
+    <span class="h-px w-4 border-t border-t-gray-300" />
+  </button>
 
-  <div v-if="isOpened.valueOf()" class="border-2 border-gray-300 rounded-md bg-licorice text-gray-300" :class="navMenuClass">
+  <div id="nav-menu" popover class="border-2 border-gray-300 rounded-md bg-licorice text-gray-300">
     <ul class="p-1">
       <li v-for="item in navItems" :key="item.name" class="focus:hover:underline p-1">
         <NuxtLink :to="item.url">
@@ -77,3 +36,13 @@ function onClick () {
     </ul>
   </div>
 </template>
+
+<style>
+#nav-menu {
+  animation: fadeIn 0.3s ease-in-out;
+  margin: 0;
+  position: fixed;
+  top: 20px;
+  left: calc(100% - 120px);
+}
+</style>
