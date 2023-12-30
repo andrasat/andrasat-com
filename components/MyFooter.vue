51209870ca58efb1b945478d99d81bd74f050289
andrasat <andra.satria1@gmail.com>
2023-12-30 23:18:31 +0700

    chore: used default devicons i tag for icons, used email png for email icon, adjust content

--- components/MyFooter.vue
+++ components/MyFooter.vue
@@ -6,13 +6,13 @@
 
     <div class="mx-auto my-2 md:my-3 flex flex-row">
       <a class="m-2 md:m-3" href="mailto:andra.satria1@gmail.com">
-        <nuxt-icon name="mail" />
+        <img alt="mail" src="~/assets/images/email.png" width="30" height="30">
       </a>
       <a class="m-2 md:m-3" href="https://www.linkedin.com/in/andrasat/">
-        <nuxt-icon name="linkedin" />
+        <i class="devicon-linkedin-plain" />
       </a>
       <a class="m-2 md:m-3" href="https://github.com/andrasat">
-        <nuxt-icon name="github" />
+        <i class="devicon-github-original-wordmark" />
       </a>
     </div>
   </footer>
--- pages/index.vue
+++ pages/index.vue
@@ -15,12 +15,12 @@ const yoe = Math.floor((NOW.getTime() - START_DEV_WORK.getTime()) / (1000 * 3600
 <template>
   <PageContainer>
     <p class="max-w-lg">
-      Hello World!, My name is Andra Satria. Currently I'm working as a Senior Full Stack Developer at
+      Hello World!, My name is Andra Satria. Currently I&apos;m working as a Senior Full Stack Developer at
       <a ref="noopener noreferrer" href="https://www.base.co.id" target="_blank" class="text-blue-600">BASE</a>.
     </p>
     <p class="mt-6 max-w-lg">
       I am a software engineer with {{ yoe }}+ years of experience in developing web apps.
-      I have a passion for creating innovative solutions that solve real-world problems and enhance user experience.
+      I have a passion building products from 0 to 1 that solve real-world problems, also I love optimizing performance, simplify things, and improve user experience.
       I have worked with various technologies and frameworks, such as below. I am always eager to learn new skills and explore new challenges.
     </p>
 
@@ -30,10 +30,10 @@ const yoe = Math.floor((NOW.getTime() - START_DEV_WORK.getTime()) / (1000 * 3600
         Languages:
       </p>
       <div>
-        <img alt="typescript" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg" width="30" height="30" class="inline ml-3">
-        <img alt="javascript" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" width="30" height="30" class="inline ml-3">
-        <img alt="go" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original-wordmark.svg" width="30" height="30" class="inline ml-3">
-        <nuxt-icon name="rust" class="custom-icon w-[30px] h-[30px] inline ml-3" />
+        <i class="devicon-typescript-plain colored ml-3" />
+        <i class="devicon-javascript-plain colored ml-3" />
+        <i class="devicon-go-original-wordmark colored ml-3" />
+        <i class="devicon-rust-plain ml-3" />
       </div>
     </section>
 
@@ -43,15 +43,15 @@ const yoe = Math.floor((NOW.getTime() - START_DEV_WORK.getTime()) / (1000 * 3600
         Tools:
       </p>
       <div class="flex flex-row flex-wrap justify-end w-[200px] lg:w-auto h-auto">
-        <nuxt-icon name="nextjs" class="custom-icon w-[40px] h-[40px] inline ml-3 p-1" />
-        <img alt="react" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" width="40" height="40" class="inline ml-3 p-1">
-        <img alt="nodejs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" width="40" height="40" class="inline ml-3 p-1">
-        <img alt="nestjs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nestjs/nestjs-plain.svg" width="40" height="40" class="inline ml-3 p-1">
-        <img alt="nuxtjs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nuxtjs/nuxtjs-original.svg" width="40" height="40" class="inline ml-3 p-1">
-        <img alt="docker" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg" width="40" height="40" class="inline ml-3 p-1">
-        <img alt="kubernetes" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg" width="40" height="40" class="inline ml-3 p-1">
-        <img alt="google cloud" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/googlecloud/googlecloud-original.svg" width="40" height="40" class="inline ml-3 p-1">
-        <img alt="amazon web service" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original.svg" width="40" height="40" class="inline ml-3 p-1">
+        <i class="devicon-nextjs-line ml-3 p-1" />
+        <i class="devicon-react-original colored ml-3 p-1" />
+        <i class="devicon-nodejs-plain colored ml-3 p-1" />
+        <i class="devicon-nestjs-plain colored ml-3 p-1" />
+        <i class="devicon-nuxtjs-plain colored ml-3 p-1" />
+        <i class="devicon-docker-plain colored ml-3 p-1" />
+        <i class="devicon-kubernetes-plain colored ml-3 p-1" />
+        <i class="devicon-googlecloud-plain colored ml-3 p-1" />
+        <i class="devicon-amazonwebservices-original colored ml-3 p-1" />
       </div>
     </section>
 
@@ -61,27 +61,18 @@ const yoe = Math.floor((NOW.getTime() - START_DEV_WORK.getTime()) / (1000 * 3600
         Databases:
       </p>
       <div class="float-right">
-        <img alt="postgresql" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" width="30" height="30" class="inline ml-3">
-        <img alt="mysql" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" width="30" height="30" class="inline ml-3">
-        <img alt="mongodb" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" width="30" height="30" class="inline ml-3">
+        <i class="devicon-postgresql-plain colored ml-3" />
+        <i class="devicon-mysql-plain colored ml-3" />
+        <i class="devicon-mongodb-plain-wordmark colored ml-3" />
       </div>
     </section>
 
     <p class="text-center mt-24">
       This website is made with
       <br>
-      NuxtJS{{ '\xa0' }}<img alt="nuxtjs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nuxtjs/nuxtjs-original.svg" width="24" height="24" class="inline ml-2 mr-2">
+      NuxtJS&nbsp;<i class="devicon-nuxtjs-plain colored ml-2 mr-2" />
       &amp;
-      TailwindCSS{{ '\xa0' }}<img alt="tailwindcss" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" width="24" height="24" class="inline ml-2 mr-2">
+      TailwindCSS&nbsp;<i class="devicon-tailwindcss-plain colored ml-2 mr-2" />
     </p>
   </PageContainer>
 </template>
-
-<style>
-.custom-icon svg {
-  height: 30px;
-  width: 30px;
-  display: inline;
-  margin-bottom: 0;
-}
-</style>
