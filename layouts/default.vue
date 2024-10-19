2544996db06c94204bd69470379b9b455d4f36fa
andra <andra.satria1@gmail.com>
2024-10-20 00:52:44 +0700

    chore: remove vercel deployment

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,7 @@
-<script setup lang="ts">
-import { SpeedInsights } from '@vercel/speed-insights/vue'
-</script>
-
 <template>
   <div class="dark:bg-gray-900 dark:text-gray-300 relative pt-16">
     <MyHeader />
     <slot />
     <MyFooter />
   </div>
-
-  <SpeedInsights />
 </template>
