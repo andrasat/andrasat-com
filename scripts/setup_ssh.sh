3403106efab0903a9e69697b131695c8beb98e87
andra <andra.satria1@gmail.com>
2024-10-20 17:09:49 +0700

    chore: update github workflow and setup_ssh

--- scripts/setup_ssh.sh
+++ scripts/setup_ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -v -m 700 $HOME/.ssh
-ssh-keyscan -H $SSH_HOST > $HOME/.ssh/known_hosts
+ssh-keyscan -H $HOST > $HOME/.ssh/known_hosts
 echo "$KEY" > $HOME/.ssh/id_rsa
 chmod 400 $HOME/.ssh/id_rsa
